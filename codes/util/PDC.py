649ba78c6e3ae0be1f4431f8a2d6d49d0264d25a
BKAUTO <wubingkun1603@gmail.com>
2021-12-09 18:35:35 +0100

    channel selection

---
+++ codes/util/PDC.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import math
+import numpy as np
+from scipy import linalg, fftpack
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import normalize
+
+class ChannelSelection():
+    def BIC(self, X, p_max=20):
+        p, bic = self.compute_order(X, p_max=p_max)
+
+        plt.figure()
+        plt.plot(np.arange(p_max + 1), bic)
+        plt.xlabel('order')
+        plt.ylabel('BIC')
+        plt.savefig('BIC.png')
+
+        return p
+
+    def compute_order(self, X, p_max):
+        """Estimate AR order with BIC
+        Parameters
+        ----------
+        X : ndarray, shape (N, n)
+            The N time series of length n
+        p_max : int
+            The maximum model order to test
+        Returns
+        -------
+        p : int
+            Estimated order
+        bic : ndarray, shape (p_max + 1,)
+            The BIC for the orders from 0 to p_max.
+        """
+        N, n = X.shape
+
+        bic = np.empty(p_max + 1)
+        bic[0] = np.inf # XXX
+
+        Y = X.T
+
+        for p in range(1, p_max + 1):
+            # print(p)
+            A, sigma = self.mvar_fit(X, p)
+            A_2d = np.concatenate(A, axis=1)
+
+            n_samples = n - p
+            bic[p] = n_samples * N * math.log(2. * math.pi)
+            bic[p] += n_samples * np.log(linalg.det(sigma))
+            bic[p] += p * (N ** 2) * math.log(n_samples)
+
+            sigma_inv = linalg.inv(sigma)
+            S = 0.
+            for i in range(p, n):
+                res = Y[i] - np.dot(A_2d, Y[i - p:i][::-1, :].ravel())
+                S += np.dot(res, sigma_inv.dot(res))
+
+            bic[p] += S
+
+        max = np.inf
+        p = 0
+        for i in range(2, p_max + 1):
+            if not np.isnan(bic[i]) and bic[i] < max:
+                max = bic[i]
+                p = i   
+        print(bic)
+        # p = np.argmin(bic)
+        return p, bic
+    
+    def cov(self, X, p):
+        """vector autocovariance up to order p
+        Parameters
+        ----------
+        X : ndarray, shape (N, n)
+            The N time series of length n
+        Returns
+        -------
+        R : ndarray, shape (p + 1, N, N)
+            The autocovariance up to order p
+        """
+        N, n = X.shape
+        R = np.zeros((p + 1, N, N))
+        for k in range(p + 1):
+            R[k] = (1. / float(n - k)) * np.dot(X[:, :n - k], X[:, k:].T)
+        return R
+
+    def mvar_fit(self, X, p):
+        """Fit MVAR model of order p using Yule Walker
+        Parameters
+        ----------
+        X : ndarray, shape (N, n)
+            The N time series of length n
+        n_fft : int
+            The length of the FFT
+        Returns
+        -------
+        A : ndarray, shape (p, N, N)
+            The AR coefficients where N is the number of signals
+            and p the order of the model.
+        sigma : array, shape (N,)
+            The noise for each time series
+        """
+        N, n = X.shape
+        gamma = self.cov(X, p)  # gamma(r,i,j) cov between X_i(0) et X_j(r)
+        G = np.zeros((p * N, p * N))
+        gamma2 = np.concatenate(gamma, axis=0)
+        gamma2[:N, :N] /= 2.
+
+        for i in range(p):
+            G[N * i:, N * i:N * (i + 1)] = gamma2[:N * (p - i)]
+
+        G = G + G.T  # big block matrix
+
+        gamma4 = np.concatenate(gamma[1:], axis=0)
+
+        phi = linalg.solve(G, gamma4)  # solve Yule Walker
+
+        tmp = np.dot(gamma4[:N * p].T, phi)
+        sigma = gamma[0] - tmp - tmp.T + np.dot(phi.T, np.dot(G, phi))
+
+        phi = np.reshape(phi, (p, N, N))
+        for k in range(p):
+            phi[k] = phi[k].T
+
+        return phi, sigma
+
+    def spectral_density(self, A, n_fft=None):
+        """Estimate PSD from AR coefficients
+        Parameters
+        ----------
+        A : ndarray, shape (p, N, N)
+            The AR coefficients where N is the number of signals
+            and p the order of the model.
+        n_fft : int
+            The length of the FFT
+        Returns
+        -------
+        fA : ndarray, shape (n_fft, N, N)
+            The estimated spectral density.
+        """
+        p, N, N = A.shape
+        if n_fft is None:
+            n_fft = max(int(2 ** math.ceil(np.log2(p))), 512)
+        A2 = np.zeros((n_fft, N, N))
+        A2[1:p + 1, :, :] = A  # start at 1 !
+        fA = fftpack.fft(A2, axis=0)
+        freqs = fftpack.fftfreq(n_fft)
+        I = np.eye(N)
+
+        for i in range(n_fft):
+            fA[i] = linalg.inv(I - fA[i])
+
+        return fA, freqs
+    
+    def PDC(self, A, sigma=None, n_fft=None):
+        """Partial directed coherence (PDC)
+        Parameters
+        ----------
+        A : ndarray, shape (p, N, N)
+            The AR coefficients where N is the number of signals
+            and p the order of the model.
+        sigma : array, shape (N,)
+            The noise for each time series.
+        n_fft : int
+            The length of the FFT.
+        Returns
+        -------
+        P : ndarray, shape (n_fft, N, N)
+            The estimated PDC.
+        """
+        p, N, N = A.shape
+
+        if n_fft is None:
+            n_fft = max(int(2 ** math.ceil(np.log2(p))), 512)
+
+        H, freqs = self.spectral_density(A, n_fft)
+        P = np.zeros((n_fft, N, N))
+
+        if sigma is None:
+            sigma = np.ones(N)
+
+        for i in range(n_fft):
+            B = H[i]
+            B = linalg.inv(B)
+            V = np.abs(np.dot(B.T.conj(), B * (1. / sigma[:, None])))
+            V = np.diag(V)  # denominator squared
+            P[i] = np.abs(B * (1. / np.sqrt(sigma))[None, :]) / np.sqrt(V)[None, :]
+
+        return P, freqs
+
+    def select(self, X, select_num=10):
+        p = self.BIC(X)
+        print(p)
+        A_est, sigma = self.mvar_fit(X, p)
+        sigma = np.diag(sigma)
+        P, freqs = self.PDC(A_est, sigma)
+        m, N, N = P.shape
+        pdc = np.zeros((N,N))
+        for i in range(N):
+            for j in range(N):
+                pdc[i, j] = np.mean(P[freqs >= 0, i, j])
+        pdc_avg = []
+        pdc_sum = pdc.sum(axis=0)
+        for i in range(N):
+            pdc_avg.append((pdc_sum[i]-pdc[i,i])/(N-1))
+        channel_selected = np.argpartition(pdc_avg, -select_num)[-select_num:]
+        return channel_selected
--- codes/util/data_loader.py
+++ codes/util/data_loader.py
@@ -2,28 +2,39 @@ from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from torchvision import transforms
 import sys
+import pyedflib
 import scipy.io as sio
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
-# from transforms import filterBank
+from picard import picard
+from scipy.stats import skew, iqr
+from sklearn.cluster import KMeans
+from sklearn.decomposition import FastICA
+from transforms import filterBank
 # from transforms import MTF
+
+from PDC import ChannelSelection
 
 import tsia.plot
 
 
 class BCI2aDataset(Dataset):
-    def get_subject_data(self, subject, path, No_channels, No_trials, Window_Length, sample_ratio=1, label=1, train=True):
+    def get_subject_data(self, subject, path, No_channels, No_trials, Window_Length, sample_ratio=1, label=1, train='train'):
         No_valid_trial = 0
         data_return = np.zeros((No_trials, No_channels, Window_Length))
         class_return = np.zeros(No_trials)
 
-        if train:
+        if train == 'train':
             data = sio.loadmat(path+'A0'+str(subject)+'T.mat')['data']
         else:
             data = sio.loadmat(path+'A0'+str(subject)+'E.mat')['data']
-       
-        for i in range(3, 3+No_trials//48):
+
+        if subject == 4:
+            start_index = 1
+        else:
+            start_index = 3
+        for i in range(start_index, start_index+No_trials//48):
             run = [data[0,i][0,0]][0]
             run_X = run[0]
             run_trial = run[1]
@@ -41,53 +52,223 @@ class BCI2aDataset(Dataset):
 
         return data_return, class_return
 
-    def __init__(self, subject, path, train=True, transform=None):
+    def __init__(self, subject, path, train='train', transform=None):
         self.transform = transform
         No_channels = 22
         No_trials = 1*48
         Window_Length = int(4.5*250)
-
-        self.data_return, self.class_return = self.get_subject_data(subject, path, No_channels, No_trials, Window_Length, sample_ratio=1, label=1, train=train)
-        if train:
-            for i in [x for x in range(1,6) if x != subject and x != 4]:
+        
+        if train == 'train':
+            self.data_return, self.class_return = self.get_subject_data(subject, path, No_channels, No_trials, Window_Length, sample_ratio=1, label=1, train=train)
+            self.plot(self.data_return, "raw.png")
+            # self.data_return = self.KMAR(self.data_return)
+            # self.plot(self.data_return, "KMAR.png")
+            for i in [x for x in range(1,6) if x != subject]:
                 negative_data, negative_class = self.get_subject_data(i, path, No_channels, No_trials, Window_Length, sample_ratio=3, label=0, train=train)
+                # negative_data = self.KMAR(negative_data)
                 self.data_return = np.concatenate((self.data_return, negative_data), axis=0)
                 self.class_return = np.concatenate((self.class_return, negative_class), axis=0)
-        else:
-            for i in [x for x in range(1,6) if x != subject and x != 4]:
+        elif train == 'intra_test':
+            self.data_return, self.class_return = self.get_subject_data(subject, path, No_channels, No_trials, Window_Length, sample_ratio=1, label=1, train=train)
+            for i in [x for x in range(1,6) if x != subject]:
                 negative_data, negative_class = self.get_subject_data(i, path, No_channels, No_trials, Window_Length, sample_ratio=3, label=0, train=train)
+                # negative_data = self.KMAR(negative_data)
                 self.data_return = np.concatenate((self.data_return, negative_data), axis=0)
                 self.class_return = np.concatenate((self.class_return, negative_class), axis=0)
+        elif train == 'inter_test':
+            self.data_return, self.class_return = self.get_subject_data(subject, path, No_channels, No_trials, Window_Length, sample_ratio=1, label=1, train=train)
+            for i in [x for x in range(6,10) if x != subject]:
+                negative_data, negative_class = self.get_subject_data(i, path, No_channels, No_trials, Window_Length, sample_ratio=3, label=0, train=train)
+                # negative_data = self.KMAR(negative_data)
+                self.data_return = np.concatenate((self.data_return, negative_data), axis=0)
+                self.class_return = np.concatenate((self.class_return, negative_class), axis=0)
+    
+    def KMAR(self, data):
+        data = np.transpose(data, (1, 0, 2))
+        data = np.reshape(data, (data.shape[0], data.shape[1]*data.shape[2]))
+        # K, W, Y = picard(data)
+        data = np.transpose(data, (1, 0))
+        ica = FastICA(n_components=data.shape[1])
+        ica.fit(data)
+        Y = ica.fit_transform(data)
+        self.plot(Y, "ICA_component.png")
+        Y_var = np.var(Y, axis=0, keepdims=True)
+        Y_skew = skew(Y, axis=0).reshape((-1, Y.shape[1]))
+        Y_iqr = iqr(Y, axis=0, interpolation='midpoint', keepdims=True)
+        features = np.concatenate((Y_var, Y_skew, Y_iqr), axis=0)
+        features = np.transpose(features, (1,0))
+        kmeans = KMeans(n_clusters=2, random_state=0).fit(features)
+        artif_label = kmeans.labels_[np.argmax(Y_var)]
+        for i, label in enumerate(kmeans.labels_):
+            if (label == artif_label):
+                Y[:, i] = 0
+        data = ica.inverse_transform(Y)
+        data = np.transpose(data, (1,0))
+        data = np.reshape(data, (data.shape[0], data.shape[1]//1125, 1125))
+        data = np.transpose(data, (1, 0, 2))
+        return data
 
-        
-        
     def __len__(self):
         return self.data_return.shape[0]
 
     def __getitem__(self, index):
-        data = self.data_return[index,:,:1125]
+        data = self.data_return[index,:,:1124]
         if self.transform:
             data = self.transform(data)
         label = self.class_return[index]
 
         return data, int(label)
+    
+    def plot(self, data, title):
+        # data = np.transpose(data, (1,0))
+        fig, axs = plt.subplots(22, 1, figsize=(20,40))
+        for i in range(22):
+            if title == "ICA_component.png":
+                axs[i].plot(data[:, i])
+            else:
+                axs[i].plot(data[44, i, 400:500])
+        fig.savefig(title)
 
+class PhysioDataset(Dataset):
+    def get_subject_data(self, subject, path, No_channels, Window_Length=640, label=1, train='train'):
+        if train == 'train':
+            run = ["04", "06", "08", "10"]
+        else:
+            run = ["12", "14"]
+        
+        data_run = []
+
+        for run_num in run:
+            if (subject < 10):
+                file = pyedflib.EdfReader(path+"/S00"+str(subject)+"/"+"S00"+str(subject)+"R"+run_num+".edf")
+            else:
+                file = pyedflib.EdfReader(path+"/S0"+str(subject)+"/"+"S0"+str(subject)+"R"+run_num+".edf")
+            annotation = file.readAnnotations()
+            marker = []
+            for i in annotation[1]:
+                marker.append(i*160)
+            y = []
+            for counter, dataPoints in enumerate(marker):
+                for i in range(int(dataPoints)):
+                    code = annotation[2][counter]
+                    if code == 'T0':
+                        y.append(0)
+                    elif code == 'T1':
+                        y.append(1)
+                    elif code == 'T2':
+                        y.append(2)
+                    else:
+                        #TODO
+                        print("catch error here")
+            totalSignals = file.signals_in_file #totalSignals = 64
+            signal_labels = file.getSignalLabels() #label names of electrode in 10-10 system
+            trial = 0
+            trial_segment = []
+            if y[0] != 0:
+                trial = 1
+                trial_segment.append(0)
+            for i in range(1, len(y)):
+                if y[i] != y[i-1] and y[i] != 0:
+                    trial = trial + 1
+                    trial_segment.append(i)
+            data = np.zeros((trial, totalSignals, 640))
+            for i in range(trial):
+                for j in np.arange(totalSignals):
+                    data[i, j, :] = file.readSignal(j)[trial_segment[i]:trial_segment[i]+640]
+            data_run.append(data)
+        
+        data_return = data_run[0]
+        for i in range(1, len(data_run)):
+            data_return = np.concatenate((data_return, data_run[i]), axis=0)
+        class_return = np.zeros(data_return.shape[0])
+        for i in range(class_return.shape[0]):
+            class_return[i] = label
+
+        return data_return, class_return
+    
+    def __init__(self, subject, path, train='train', transform=None):
+        self.transform = transform
+        No_channels = 64
+
+        if train == 'train':
+            self.data_return, self.class_return = self.get_subject_data(subject, path, No_channels, label=1, train=train)
+            # self.plot(self.data_return, "raw.png")
+            self.data_return = self.KMAR(self.data_return)
+            # self.plot(self.data_return, "KMAR.png")
+            channelSelection = ChannelSelection()
+            channel_selected = channelSelection.select(self.data_return[20,:,:])
+            np.savetxt('test.npy', self.data_return[20,:,:])
+            b = np.loadtxt('test.npy')
+            print(self.data_return[20,:,:]==b)
+            print(channel_selected)
+            for i in [x for x in range(1,6) if x != subject]:
+                negative_data, negative_class = self.get_subject_data(i, path, No_channels, label=0, train=train)
+                negative_data = self.KMAR(negative_data)
+                self.data_return = np.concatenate((self.data_return, negative_data[:20, :, :]), axis=0)
+                self.class_return = np.concatenate((self.class_return, negative_class[:20]), axis=0)
+        elif train == 'intra_test':
+            self.data_return, self.class_return = self.get_subject_data(subject, path, No_channels, label=1, train=train)
+            for i in [x for x in range(1,6) if x != subject]:
+                negative_data, negative_class = self.get_subject_data(i, path, No_channels, label=0, train=train)
+                negative_data = self.KMAR(negative_data)
+                self.data_return = np.concatenate((self.data_return, negative_data), axis=0)
+                self.class_return = np.concatenate((self.class_return, negative_class), axis=0)
+        elif train == 'inter_test':
+            self.data_return, self.class_return = self.get_subject_data(subject, path, No_channels, label=1, train=train)
+            for i in [x for x in range(6,10) if x != subject]:
+                negative_data, negative_class = self.get_subject_data(i, path, No_channels, label=0, train=train)
+                negative_data = self.KMAR(negative_data)
+                self.data_return = np.concatenate((self.data_return, negative_data), axis=0)
+                self.class_return = np.concatenate((self.class_return, negative_class), axis=0)
+
+    def __len__(self):
+        return self.data_return.shape[0]
+    
+    def __getitem__(self, index):
+        data = self.data_return[index,:,:640]
+        if self.transform:
+            data = self.transform(data)
+        label = self.class_return[index]
+
+        return data, int(label)
+    
+    def KMAR(self, data):
+        data = np.transpose(data, (1, 0, 2))
+        data = np.reshape(data, (data.shape[0], data.shape[1]*data.shape[2]))
+        # K, W, Y = picard(data)
+        data = np.transpose(data, (1, 0))
+        ica = FastICA(n_components=data.shape[1])
+        ica.fit(data)
+        Y = ica.fit_transform(data)
+        # self.plot(Y, "ICA_component.png")
+        Y_ptp = np.ptp(Y, axis=0, keepdims=True)
+        Y_skew = skew(Y, axis=0).reshape((-1, Y.shape[1]))
+        Y_iqr = iqr(Y, axis=0, interpolation='midpoint', keepdims=True)
+        features = np.concatenate((Y_ptp, Y_skew, Y_iqr), axis=0)
+        features = np.transpose(features, (1,0))
+        kmeans = KMeans(n_clusters=2, random_state=0).fit(features)
+        artif_label = np.bincount(kmeans.labels_).argmin()
+        for i, label in enumerate(kmeans.labels_):
+            if (label == artif_label):
+                Y[:, i] = 0
+        data = ica.inverse_transform(Y)
+        data = np.transpose(data, (1,0))
+        data = np.reshape(data, (data.shape[0], data.shape[1]//640, 640))
+        data = np.transpose(data, (1, 0, 2))
+        return data
+
+    def plot(self, data, title):
+        # data = np.transpose(data, (1,0))
+        fig, axs = plt.subplots(64, 1, figsize=(20,40))
+        for i in range(64):
+            if title == "ICA_component.png":
+                axs[i].plot(data[:, i])
+            else:
+                axs[i].plot(data[44, i, :])
+        fig.savefig(title)
+    
 if __name__ == '__main__':
-    filterTransform = filterBank([[4,8],[8,12],[12,16],[16,20],[20,24],[24,28],[28,32],[32,36],[36,40]], 250)
-    mtf = MTF(bins=8, image_size=128)
-    transform=transforms.Compose([filterTransform, mtf])
-    train_data = BCI2aDataset(3, "../../data/", train=True, transform=transform)
+    filterTransform = filterBank([[4,8],[8,12],[12,16],[16,20],[20,24],[24,28],[28,32],[32,36],[36,40]], 160)
+
+    train_data = PhysioDataset(1, "../../data/physionet/physionet.org/files/eegmmidb/1.0.0", train='train', transform=filterTransform)
     train_dataloader = DataLoader(train_data, batch_size=1, shuffle=True)
-
-    data, label = next(iter(train_dataloader))
-    # data = data.numpy().reshape(data.shape[1], data.shape[2], data.shape[3])
-    # data = np.swapaxes(data, 1, 2)
-    # plt.plot(data[0,8,:])
-    # plt.savefig("data")
-    data = np.squeeze(data.numpy())
-
-    fig = plt.figure(figsize=(5,4))
-    ax = fig.add_subplot(111)
-    _, mappable_image = tsia.plot.plot_markov_transition_field(mtf=data[19,5], ax=ax, reversed_cmap=True)
-    plt.colorbar(mappable_image)
-    plt.savefig("mappable_image")
